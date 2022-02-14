51c7da5ec872f600d89ab566e25533e959765a87
Qianjiachen55 <qianjiachen@happywater.xyz>
2022-02-14 11:11:30 +0800

    feat main.go

--- user/go.mod
+++ user/go.mod
@@ -3,11 +3,10 @@ module github.com/Qianjiachen55/micro-todoList
 go 1.16
 
 require (
-	github.com/golang/protobuf v1.5.2
-	github.com/micro/go-micro/v2 v2.9.1
-	github.com/spf13/viper v1.10.1
-	golang.org/x/crypto v0.0.0-20220210151621-f4118a5b28e2
-	google.golang.org/protobuf v1.27.1
-	gorm.io/driver/mysql v1.2.3
-	gorm.io/gorm v1.22.5
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/micro/go-micro/v2 v2.9.1 // indirect
+	golang.org/x/crypto v0.0.0-20220213190939-1e6e3497d506 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+	gorm.io/driver/mysql v1.2.3 // indirect
+	gorm.io/gorm v1.22.5 // indirect
 )
--- user/main.go
+++ user/main.go
@@ -1,1 +1,26 @@
-package user
+package main
+
+import (
+	"github.com/Qianjiachen55/micro-todoList/core"
+	"github.com/Qianjiachen55/micro-todoList/services"
+	"github.com/micro/go-micro/v2"
+	"github.com/micro/go-micro/v2/registry"
+	"github.com/micro/go-micro/v2/registry/etcd"
+)
+
+func main()  {
+	etcdReg := etcd.NewRegistry(
+		registry.Addrs("127.0.0.1:2379"),
+		)
+
+	microServer := micro.NewService(
+		micro.Name("rpcUserService"),
+		micro.Address("127.0.0.1:8082"),
+		micro.Registry(etcdReg),
+		)
+	microServer.Init()
+
+	_=services.RegisterUserServiceHandler(microServer.Server(),new(core.UserService))
+	_=microServer.Run()
+
+}
